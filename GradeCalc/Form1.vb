aef324c2fa8617789844d4bbbc40b3b42724b5f7
7webfrog <7webfrog@gmail.com>
2017-09-08 17:32:39 -0400

    Designed initial CalcE1 UI (again)

--- GradeCalc/Form1.vb
+++ GradeCalc/Form1.vb
@@ -12,4 +12,8 @@
         Dim F1 As Decimal = GP1A + GP2A + E1A
         txtboxOutF1.Text = F1
     End Sub
+
+    Private Sub BtnExeE1_Click(sender As Object, e As EventArgs) Handles BtnExeE1.Click
+
+    End Sub
 End Class
