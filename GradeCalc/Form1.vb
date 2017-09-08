c69f02711e46982dea43bcb248d71c583a8c3977
7webfrog <7webfrog@gmail.com>
2017-09-07 22:37:05 -0400

    Added support for E1 Grade calculation in design.

--- GradeCalc/Form1.vb
+++ GradeCalc/Form1.vb
@@ -1,22 +1,22 @@
 ﻿Public Class Form1
-    Private Sub BtnExe_Click(sender As Object, e As EventArgs) Handles BtnExe.Click
+    Private Sub BtnExeF1_Click(sender As Object, e As EventArgs) Handles BtnExeF1.Click
         Dim GP1 As Integer
-        Convert.ToInt32(txtboxGP1.Text)
-        GP1 = txtboxGP1.Text
+        Convert.ToInt32(txtboxGP1F1.Text)
+        GP1 = txtboxGP1F1.Text
         Dim GP1A As Decimal
         GP1A = GP1 * 0.4
         Dim GP2 As Integer
-        Convert.ToInt32(txtboxGP2.Text)
-        GP2 = txtboxGP2.Text
+        Convert.ToInt32(txtboxGP2F1.Text)
+        GP2 = txtboxGP2F1.Text
         Dim GP2A As Decimal
         GP2A = GP2 * 0.4
         Dim E1 As Integer
-        Convert.ToInt32(txtboxE1.Text)
-        E1 = txtboxE1.Text
+        Convert.ToInt32(txtboxE1F1.Text)
+        E1 = txtboxE1F1.Text
         Dim E1A As Decimal
         E1A = E1 * 0.2
         Dim F1 As Decimal
         F1 = GP1A + GP2A + E1A
-        txtboxOut.Text = F1
+        txtboxOutF1.Text = F1
     End Sub
 End Class
