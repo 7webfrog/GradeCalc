9f45688e068d4bddc095439c85e93014db31e710
7webfrog <7webfrog@gmail.com>
2017-09-09 22:19:47 -0400

    Revert "Patch 4"

--- GradeCalc/Form1.vb
+++ GradeCalc/Form1.vb
@@ -1,15 +1,22 @@
 ﻿Public Class Form1
-    Private Sub BtnExeF1_Click(sender As Object, e As EventArgs) Handles BtnExeF1.Click
-        Convert.ToInt32(txtboxGP1F1.Text)
-        Dim GP1 As Integer = txtboxGP1F1.Text
-        Dim GP1A As Decimal = GP1 * 0.4
-        Convert.ToInt32(txtboxGP2F1.Text)
-        Dim GP2 As Integer = txtboxGP2F1.Text
-        Dim GP2A As Decimal = GP2 * 0.4
-        Convert.ToInt32(txtboxE1F1.Text)
-        Dim E1 As Integer = txtboxE1F1.Text
-        Dim E1A As Decimal = E1 * 0.2
-        Dim F1 As Decimal = GP1A + GP2A + E1A
-        txtboxOutF1.Text = F1
+    Private Sub BtnExe_Click(sender As Object, e As EventArgs) Handles BtnExe.Click
+        Dim GP1 As Integer
+        Convert.ToInt32(txtboxGP1.Text)
+        GP1 = txtboxGP1.Text
+        Dim GP1A As Decimal
+        GP1A = GP1 * 0.4
+        Dim GP2 As Integer
+        Convert.ToInt32(txtboxGP2.Text)
+        GP2 = txtboxGP2.Text
+        Dim GP2A As Decimal
+        GP2A = GP2 * 0.4
+        Dim E1 As Integer
+        Convert.ToInt32(txtboxE1.Text)
+        E1 = txtboxE1.Text
+        Dim E1A As Decimal
+        E1A = E1 * 0.2
+        Dim F1 As Decimal
+        F1 = GP1A + GP2A + E1A
+        txtboxOut.Text = F1
     End Sub
 End Class
