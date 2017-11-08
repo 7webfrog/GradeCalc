4b0701c64f6a16041c877a3c8729162a9e5c012e
7webfrog <7webfrog@gmail.com>
2017-11-08 15:29:59 -0500

    Decreased RAM usage (minor fix)
    Changed "Me.Hide()" to "Me.Close()" so form doesn't run in background

--- GradeCalc/InitForm.vb
+++ GradeCalc/InitForm.vb
@@ -4,11 +4,11 @@
     'Startup form; gives the user option of which calculator to use
     Private Sub Final_Click(sender As Object, e As EventArgs) Handles btnFinal.Click 'Final Grade
         Form1.Show()
-        Me.Hide()
+        Me.Close()
     End Sub
 
     Private Sub Exam_Click(sender As Object, e As EventArgs) Handles btnExam.Click 'Exam Grade
         Form2.Show()
-        Me.Hide()
+        Me.Close()
     End Sub
 End Class
